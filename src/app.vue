2a079de8199a5161e7c28dde181439567ff02c97
lzy <1203986130@qq.com>
2019-04-02 10:53:17 +0800

    搭建好的最完成基础vue框架,接下来要对项目结构进行优化

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <h2 class="color-red">{{ text }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data() {
+            return {
+                text: "Hello, Vue!"
+            };
+        }
+    };
+</script>
+
+<style scoped>
+    .color-red {
+        color: green;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'; // 从node_modules引入vue类库
+import App from './app.vue'; // ES6 语法，相当于 import { default as App } from './app.vue'。因为app.vue用过的是export default {...}，所以可以这样写
+import './assets/styles/global.scss';
+new Vue({
+    el: '#app',
+    components: {
+        App
+    },
+    /**
+     * Vue2.x 区分了完整版和运行时版，然后当使用 webpack 的时候默认是只包含运行时的 ES Module 版本。
+     * 运行时版不包括编译器【用来将模板字符串编译成为 JavaScript 渲染函数的代码】，所以不能用 template，要用 render 函数
+     * 参考：https://cn.vuejs.org/v2/guide/installation.html#%E5%AF%B9%E4%B8%8D%E5%90%8C%E6%9E%84%E5%BB%BA%E7%89%88%E6%9C%AC%E7%9A%84%E8%A7%A3%E9%87%8A
+     */
+    render: (createElement) => createElement(App)
+});
+// 测试babel-loader
+    class Person {
+        constructor(name, age) {
+                    this.name = name;
+                    this.age = age;
+                }
+
+        sayHello() {
+                    console.log(`Hello, my name is ${this.name}`);
+                }
+    }
+
+const person1 = new Person("刘忠岳", 28);
+person1.sayHello();
